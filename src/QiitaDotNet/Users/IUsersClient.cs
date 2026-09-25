42e25b9934225987d1fd260139c10a467d916b19
tacbooon <tacbooon@icloud.com>
2026-09-26 07:38:08 +0900

    API リファレンスを微調整
    - ArgumentOutOfRangeException をより抽象的な ArgumentException に変更
    - QiitaClient のサンプルコードのビルドが通らない問題を修正

--- src/QiitaDotNet/Users/IUsersClient.cs
+++ src/QiitaDotNet/Users/IUsersClient.cs
@@ -15,7 +15,7 @@ public interface IUsersClient
     /// <param name="perPage">1ページあたりの件数 (1〜100)。省略時はサーバー既定値の 20。</param>
     /// <param name="cancellationToken">キャンセル用トークン。</param>
     /// <returns>ユーザーの一覧。</returns>
-    /// <exception cref="ArgumentOutOfRangeException"><paramref name="page"/> または <paramref name="perPage"/> が範囲外。</exception>
+    /// <exception cref="ArgumentException"><paramref name="page"/> または <paramref name="perPage"/> が範囲外。</exception>
     /// <exception cref="HttpRequestException">ネットワークエラー。</exception>
     /// <exception cref="JsonException">成功レスポンスのボディが不正な JSON。</exception>
     /// <exception cref="OperationCanceledException">キャンセルまたはタイムアウト。</exception>
